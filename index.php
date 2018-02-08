9c8d14019dab6ae3b600b4f92474f9b2691ef1b1
herve bassinot <herve.bassinot@savoirfairelinux.com>
2018-02-07 21:56:04 -0500

    add google analytics script

--- index.php
+++ index.php
@@ -22,6 +22,17 @@
       <link rel="stylesheet" href="css/main.css" />
 
       <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113804162-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-113804162-1');
+      </script>
+
   </head>
 
   <body>
