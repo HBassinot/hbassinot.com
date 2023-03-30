b91c19ba12746030553f101d5a4369980d17bdad
hbassinot <herve.bassinot@gmail.com>
2023-03-30 10:48:30 -0400

    change path

--- 404.php
+++ 404.php
@@ -17,14 +17,14 @@
       <meta name="category"     content="<?php echo $lang_category ?>">
       <meta name="Description"  content="<?php echo $lang_description ?>">
 
-      <script src="js/main.js"></script>
+      <script src="/js/main.js"></script>
 
       <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick.css"/>
       <link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick-theme.css"/>
       <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
       <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato">
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" type="text/css" href="css/main.css" />
+      <link rel="stylesheet" type="text/css" href="/css/main.css" />
 
       <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
       <link rel="canonical" href="https://hbassinot.com"/>
