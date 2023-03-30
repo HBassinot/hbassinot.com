333774c430e0776f36d7d90c84a4464d6bb283f3
hbassinot <herve.bassinot@gmail.com>
2023-03-30 10:41:11 -0400

    absolute path

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       <link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
       <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato">
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" type="text/css" href="css/main.css" />
+      <link rel="stylesheet" type="text/css" href="/css/main.css" />
 
       <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
 			<link rel="canonical" href="https://hbassinot.com"/>
